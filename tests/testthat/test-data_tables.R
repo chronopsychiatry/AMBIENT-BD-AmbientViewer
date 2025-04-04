6f1c78c3210dd040561ab9bf7189d832f5fdd08c
DanielThedie <dthedie@gmail.com>
2025-04-04 15:09:51 +0100

    Fix some CI issues

--- tests/testthat/test-data_tables.R
+++ tests/testthat/test-data_tables.R
@@ -13,9 +13,9 @@ test_that("get_duplicate_sessions returns duplicates correctly", {
   expected <- tibble::tibble(
     session_id = c(1, 2, 5, 6),
     session_start = as.POSIXct(c("2025-03-03T12:00:00", "2025-03-03T20:00:00", "2025-03-06T12:00:00",
-                                 "2025-03-06T14:00:00"), format = "%Y-%m-%dT%H:%M:%OS"),
+                                 "2025-03-06T14:00:00"), format = "%Y-%m-%dT%H:%M:%OS", tz = "UTC"),
     session_end = as.POSIXct(c("2025-03-03T14:00:00", "2025-03-04T17:00:00", "2025-03-06T14:00:00",
-                               "2025-03-06T16:00:00"), format = "%Y-%m-%dT%H:%M:%OS"),
+                               "2025-03-06T16:00:00"), format = "%Y-%m-%dT%H:%M:%OS", tz = "UTC"),
     night = as.Date(c("2025-03-03", "2025-03-03", "2025-03-06",
                       "2025-03-06")),
     session_duration_hours = c(2, 21, 2, 2)
--- tests/testthat/test-plot_timeseries.R
+++ tests/testthat/test-plot_timeseries.R
@@ -1,3 +1,5 @@
+source("../../R/plots/timeseries.R")
+
 mock_epochs <- data.frame(
   timestamp = c(
     "2025-03-03T11:00:00", "2025-03-03T23:00:00",
@@ -13,21 +15,9 @@ mock_epochs <- data.frame(
   adjusted_time = c(23, 11, 13, 23, 1, 11)
 )
 
-test_that("plot_timeseries filters data correctly", {
-  # Call the function with a specific date range
-  filtered_plot <- plot_timeseries(mock_epochs, "temperature_ambient_mean", "2025-03-03", "2025-03-04")
-
-  # Extract the filtered data from the ggplot object
-  filtered_data <- ggplot2::ggplot_build(filtered_plot)$data[[1]]
-
-  # Check that only data within the specified date range is included
-  expect_true(all(as.Date(filtered_data$night) >= as.Date("2025-03-03")))
-  expect_true(all(as.Date(filtered_data$night) <= as.Date("2025-03-04")))
-})
-
 test_that("plot_timeseries returns a ggplot object", {
   # Call the function
-  plot <- plot_timeseries(mock_epochs, "temperature_ambient_mean", "2025-03-03", "2025-03-04")
+  plot <- plot_timeseries(mock_epochs, "temperature_ambient_mean")
 
   # Check that the result is a ggplot object
   expect_s3_class(plot, "ggplot")
@@ -35,12 +25,12 @@ test_that("plot_timeseries returns a ggplot object", {
 
 test_that("plot_timeseries uses the correct x and y mappings", {
   # Call the function
-  plot <- plot_timeseries(mock_epochs, "temperature_ambient_mean", "2025-03-03", "2025-03-04")
+  plot <- plot_timeseries(mock_epochs, "temperature_ambient_mean")
 
   # Extract the plot data
   plot_data <- ggplot2::ggplot_build(plot)$data[[1]]
 
   # Check that the x and y mappings are correct
-  expect_equal(plot_data$x, mock_epochs$adjusted_time[2:6])
-  expect_equal(plot_data$y, mock_epochs$temperature_ambient_mean[2:6])
+  expect_equal(plot_data$x, mock_epochs$adjusted_time)
+  expect_equal(plot_data$y, mock_epochs$temperature_ambient_mean)
 })
