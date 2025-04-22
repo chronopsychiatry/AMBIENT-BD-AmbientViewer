f9ab98f18564ce262f79609877fb04f78a54dfc3
DanielThedie <dthedie@gmail.com>
2025-04-22 12:03:39 +0100

    Add logging upon data loading

--- R/module-input.R
+++ R/module-input.R
@@ -18,11 +18,13 @@ input_server <- function(id, session) {
   shiny::moduleServer(id, function(input, output, session) {
     sessions <- shiny::reactive({
       shiny::req(input$sessions_file)
+      logging::loginfo(paste0("Loading sessions file: ", input$sessions_file$name))
       load_sessions(input$sessions_file$datapath)
     })
 
     epochs <- shiny::reactive({
       shiny::req(input$epochs_file)
+      logging::loginfo(paste0("Loading epochs file: ", input$epochs_file$name))
       load_epochs(input$epochs_file$datapath)
     })
 
